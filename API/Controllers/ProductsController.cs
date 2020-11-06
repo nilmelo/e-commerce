a9ce734d039f3a6b7c71aa5eb357d6228820fcd4
NilDevStudio <nildevstudio@outlook.com>
2020-11-06 13:02:09 -0300

    padrão repositório, config, migrations e classlibs

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,7 +1,8 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using API.Data;
-using API.Models;
+using Core.Entities;
+using Core.Interfaces;
+using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,24 +12,25 @@ namespace API.Controllers
 	[Route("api/[controller]")]
 	public class ProductsController : ControllerBase
 	{
-		private readonly StoreContext _context;
-		public ProductsController(StoreContext context)
-		{
-			_context = context;
+		private readonly IProductRepository _repo;
 
+		public ProductsController(IProductRepository repo)
+		{
+			_repo = repo;
 		}
 
 		[HttpGet]
 		public async Task<ActionResult<List<Product>>> GetProducts()
 		{
-			var products = await _context.Products.ToListAsync();
+			var products = await _repo.GetProductsAsync();
 			return Ok(products);
 		}
 
 		[HttpGet("{id}")]
 		public async Task<ActionResult<Product>> GetProduct(int id)
 		{
-			return await _context.Products.FindAsync(id);
+			return await _repo.GetProductByIdAsync(id);
 		}
+
 	}
 }
--- API/Models/Product.cs
+++
@@ -1,8 +0,0 @@
-namespace API.Models
-{
-    public class Product
-    {
-        public int Id { get; set; }
-		public string Name { get; set; }
-    }
-}
