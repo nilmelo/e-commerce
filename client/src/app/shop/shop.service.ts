d5995c3d6972159442df8071cf2b9197d99f1638
NilDevStudio <nildevstudio@outlook.com>
2020-11-11 09:49:57 -0300

    shop page

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IPagination } from '../shared/models/pagination';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShopService {
+  baseUrl = 'http://localhost:5000/api/';
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getProducts() {
+	return this.http.get<IPagination>(this.baseUrl + 'products?pageSize=50');
+  }
 }
